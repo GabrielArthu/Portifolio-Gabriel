5cfdde6d87cf9a3d9ba20bc6a58f66564d3fda70
Gabriel A <gabrielarthur11333@gmail.com>
2025-12-18 04:43:25 -0300

    Refactor styles and improve layout for better responsiveness; enhance button and section styles; optimize scroll behavior and animations.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// --- Lógica do Botão Voltar ao Topo ---
     const backToTopBtn = document.getElementById('back-to-top');
 
-    // Monitora o scroll da página
     window.addEventListener('scroll', () => {
         if (window.scrollY > 300) { // Se rolar mais de 300px
             backToTopBtn.classList.add('show');
         } else {
             backToTopBtn.classList.remove('show');
         }
+
+        
     });
 
-    // Ação de clique para subir suavemente
     backToTopBtn.addEventListener('click', () => {
         window.scrollTo({
             top: 0,
